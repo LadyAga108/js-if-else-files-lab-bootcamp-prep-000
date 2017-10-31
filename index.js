5eafed3d547ced216296745a997cce1e03329f41
LadyAga108 <aga.kubica108@gmail.com>
2017-10-31 17:41:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var height = "70"
 
 if (name === "Susan") {
   console.log("The name is correct")
+} else {
+  console.log("E: S, R: J")
 }
+)
 
 if (height === "70") {
   console.log("The height is correct")
