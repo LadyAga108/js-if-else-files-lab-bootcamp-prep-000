9b081b048ba9a714e6840a3de36e6d5cf0920341
LadyAga108 <aga.kubica108@gmail.com>
2017-10-31 17:48:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 var name = "Susan"
 var height = "70"
 
-if (name === "Susan") {
-  console.log("The name is correct")
-} else {
-  console.log("E: S, R: J")
-}
-
-if (height === "70") {
-  console.log("The height is correct")
-} else {
-  console.log("E: 70, R: 74")
-}
 
 // Don't worry about this
 module.exports = { name, height
